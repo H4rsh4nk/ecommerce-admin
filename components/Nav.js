fafb36bf1200912ac122e6bd855f7ff06b1aa199
H4rsh4nk <pathakharsh13@gmail.com>
2023-06-22 06:52:34 -0400

    Integrated S3 with Products/image functionality 	modified:   components/Nav.js 	modified:   components/ProductForm.js 	new file:   components/Spinners.js 	modified:   models/Product.js 	modified:   package-lock.json 	modified:   package.json 	modified:   pages/api/products.js 	modified:   pages/api/upload.js 	new file:   pages/categories.js 	modified:   styles/globals.css

--- components/Nav.js
+++ components/Nav.js
@@ -16,7 +16,7 @@ export default function Nav() {
                     EcommerceAdmin
                 </span>
             </Link>
-            <nav className='felx flex-col gap-2'>
+            <nav className='flex flex-col gap-2'>
                 <Link href={'/'} className={pathname === '/' ? activeLink : inactiveLink}>
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
                         <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 12l8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25" />
@@ -29,6 +29,11 @@ export default function Nav() {
                     </svg>
 
                     Products
+                </Link><Link href={'/categories'} className={pathname.includes('/categories') ? activeLink : inactiveLink}>
+                   <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M8.25 6.75h12M8.25 12h12m-12 5.25h12M3.75 6.75h.007v.008H3.75V6.75zm.375 0a.375.375 0 11-.75 0 .375.375 0 01.75 0zM3.75 12h.007v.008H3.75V12zm.375 0a.375.375 0 11-.75 0 .375.375 0 01.75 0zm-.375 5.25h.007v.008H3.75v-.008zm.375 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z" />
+                    </svg>
+                    Categories
                 </Link>
                 <Link href={'/orders'} className={pathname.includes('/orders') ? activeLink : inactiveLink}>
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
---
+++ components/Spinners.js
@@ -0,0 +1,6 @@
+import { BounceLoader } from "react-spinners";
+export default function Spinner() {
+    return (
+        <BounceLoader color={'#1e3a8a'} speedMultiplier={2} />
+    );
+}
